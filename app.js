cb21aef0b4511c0b9ba7ef21cc6da74b7e6a7cd8
ykwei0 <45205623+ykwei0@users.noreply.github.com>
2019-05-21 06:56:52 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,20 @@
 class App {
   constructor() {
     // TODO(you): Implement the constructor and add fields as necessary.
+    const menuElement = document.querySelector('#menu');
+    this.menuScreen = new MenuScreen(menuElement);
+    const musicElement = document.querySelector('#music');
+    this.musicScreen = new MusicScreen(musicElement);
+
+    this.toMusic = this.toMusic.bind(this);
+    document.addEventListener('endMenu',this.toMusic);
   }
   // TODO(you): Add methods as necessary.
+  toMusic(event) {
+    this.menuScreen.hide();
+    let songURL = event.detail.songValue;
+    let gifURL = event.detail.gifData;
+    //console.log(gifURL);
+    this.musicScreen.show(songURL,gifURL);
+  }
 }
